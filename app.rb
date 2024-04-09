2f8f012637c2c67510b2de0cfb370fa66cd7fa45
DB Christenson <dbeauchristenson@gmail.com>
2024-04-09 21:06:08 +0000

    First iter fortesting

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 CHOICES = ["Rock", "Paper", "Scissors"]
+OUTCOMES = {
+  "Rock" => "Scissors",
+  "Scissors" => "Paper",
+  "Paper" => "Rock"
+}
 
 def computer_choice
   choice = CHOICES.sample
@@ -9,11 +14,15 @@ def computer_choice
 end
 
 def get_outcome(player_choice, computer_choice)
-  w = "We won!"
-  l = "We lost!"
-  t = "We tied!"
+  ws = "We won!"
+  ls = "We lost!"
+  ts = "We tied!"
   if player_choice == computer_choice
-    return ""
+    return ts
+  elsif OUTCOMES[player_choice].include?(computer_choice)
+    return ws
+  else
+    return ls
   end
 end
 
@@ -25,7 +34,7 @@ get("/rock") do
   player_choice = "Rock"
   computer_choice = computer_choice()
   outcome = get_outcome(player_choice, computer_choice)
-  
+
   erb(:rock)
 end
 
